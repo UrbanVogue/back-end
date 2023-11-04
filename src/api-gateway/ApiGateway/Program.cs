b94698db36bd4dff8bb932e0fed4ab8dedb42a28
Arsenii Merkulov <arsenii.vm@gmail.com>
2023-11-04 18:02:58 +0200

    #8 added ocelot api gateway service. Add all services to single docker-compose and docker-compose.override files

---
+++ src/api-gateway/ApiGateway/Program.cs
@@ -0,0 +1,33 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Services.AddOcelot(builder.Configuration)
+    .AddCacheManager(x => x.WithDictionaryHandle());
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+}
+
+app.UseRouting();
+
+app.UseStaticFiles();
+
+    
+app.UseOcelot().Wait();
+
+
+app.Run();
